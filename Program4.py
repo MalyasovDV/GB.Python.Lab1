2cf01ee0fb014050e213324a1ca38f178227420d
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-09-28 01:35:37 +0800

    Task done

--- Program4.py
+++ Program4.py
@@ -1,2 +1,17 @@
 def ValuesInQuadrant(quadrant):
-    
+    match quadrant:
+        case 1:
+            print(f'quadrant {quadrant}: x in (0; INF); y in (0; INF)')
+        case 2:
+            print(f'quadrant {quadrant}: x in (-INF; 0); y in (0; INF)')
+        case 3:
+            print(f'quadrant {quadrant}: x in (-INF; 0); y in (-INF; 0)')
+        case 4:
+            print(f'quadrant {quadrant}: x in (0; INF); y in (-INF; 0)')
+
+
+quadrant = 1
+ValuesInQuadrant(quadrant)
+
+quadrant = 3
+ValuesInQuadrant(quadrant)
