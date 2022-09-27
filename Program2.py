e5fef7a6b672146278dd8b8827d901c8117c2512
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-09-27 16:54:58 +0800

    Task done

---
+++ Program2.py
@@ -0,0 +1,25 @@
+def FirstExpression(x, y, z):
+    return not(x or y or z)
+
+def SecondExpression(x, y, z):
+    return (not x) and (not y) and (not z)
+
+def CheckIfBoolEquivalent(a, b):
+    if a == b:
+        return True
+    else:
+        return False
+
+
+Variables = [False, True]
+print('x  y  z  FE SE')
+for x in Variables:
+    for y in Variables:
+      for z in Variables:
+           print(int(x), '', int(y), '', int(z), '', int(FirstExpression(x, y, z)), '', int(SecondExpression(x, y, z)), CheckIfBoolEquivalent(FirstExpression(x, y, z), SecondExpression(x, y, z)))
+
+#for x in Variables:
+#    if (x):
+#        print(f'int({x})')
+#    else:
+#        print()
